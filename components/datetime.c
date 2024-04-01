10b02a7f1a820355a779876b091937144f7a219c
Kayodê Vital <kayode.vital@tutanota.com>
2024-03-31 21:42:55 -0300

    initial commit

--- components/datetime.c
+++ components/datetime.c
@@ -1,6 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 #include <stdio.h>
 #include <time.h>
+#include <locale.h>
 
 #include "../slstatus.h"
 #include "../util.h"
@@ -10,6 +11,7 @@ datetime(const char *fmt)
 {
 	time_t t;
 
+	setlocale(LC_ALL, "");
 	t = time(NULL);
 	if (!strftime(buf, sizeof(buf), fmt, localtime(&t))) {
 		warn("strftime: Result string exceeds buffer size");
